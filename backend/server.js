33bf6b28451198c416adbbf3d1df59b147961d67
gttdo <vinces.gerardo@gmail.com>
2020-12-02 19:34:51 -0800

    add: server and db files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import products from './data/products.js';
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
 
 app.get('/api/products', (req, res) => {
     res.json(products)
@@ -12,4 +23,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
